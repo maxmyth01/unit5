4d4ede894f77ae8b7f9cb152ce744b9d398f10aa
maxmyth01 <maxmyth01@gmail.com>
2017-11-13 14:38:18 -0500

    Updated from Brython Server: 11/13/2017 2:38:18 PM

--- middleWord.py
+++ middleWord.py
@@ -1,1 +1,11 @@
+#Max Low
+#11-13-17
+#middleWord.py -- finds middle if even print both
+
+words = input('Enter some words').split()
+
+if len(words)%2 == 0:
+    print(words(len(words)//2:len(words)+1))
+else:
+    print(len(words)//2)
 
