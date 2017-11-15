70c9730629062b3f829945ea57ce29f931794c63
maxmyth01 <maxmyth01@gmail.com>
2017-11-15 13:35:16 -0500

    Updated from Brython Server: 11/15/2017 1:35:15 PM

--- dictionaryDemo.py
+++ dictionaryDemo.py
@@ -1,1 +1,10 @@
+#Max Low
+#11-15-17
+#dictionaryDemo.py -- list practice
 
+dictionary = ['alphbet','sweatshirt','sweatpants','shorts','computer','waterbottle']
+
+dictionary.sort()
+
+number = int(input('What number word do you want to look up?'))
+print('Word number',number,'is'dictionary[number -= 1])
