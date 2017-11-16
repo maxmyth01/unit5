dce17f7e85ef2cccbf97cbfb641045b17bcfd0cc
maxmyth01 <maxmyth01@gmail.com>
2017-11-16 14:09:23 -0500

    Updated from Brython Server: 11/16/2017 2:09:22 PM

--- stringListDemo.py
+++ stringListDemo.py
@@ -1,1 +1,12 @@
+#Max Low
+#11-16-17
+#stringListDemo.py -- print out words that start with a vowel
 
+words = imput("Enter Some Words: ").split(" ")
+
+for word in words:
+    if word[0] in "aeiouAEIOU":
+        print(word)
+        
+
+        
