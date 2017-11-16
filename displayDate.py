c8b662440d680b9b7fadeefafcfbacbbcdae19f1
maxmyth01 <maxmyth01@gmail.com>
2017-11-16 14:49:37 -0500

    Updated from Brython Server: 11/16/2017 2:49:35 PM

--- displayDate.py
+++ displayDate.py
@@ -1,1 +1,12 @@
+#Max Low
+#11-16-17
+#display Date.py
 
+from datetime import date
+from calendar import weekday
+
+today = datetime.date.today()
+print(today)
+
+month = date.today().month 
+year = date.today().year
