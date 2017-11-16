cc53df80e82e7fe88e13b3bd6ab7feae163d76a6
maxmyth01 <maxmyth01@gmail.com>
2017-11-16 14:40:16 -0500

    Updated from Brython Server: 11/16/2017 2:40:15 PM

--- betterColoredWindow.py
+++ betterColoredWindow.py
@@ -1,1 +1,26 @@
+#Max Low
+#11-16-17
+#betterColoredWindow.py
 
+#Max Low
+#10-16-17
+#colorChangeWindow.py -- every click changes the color
+
+from ggame import *
+from random import randint
+
+
+color = [Color(0x0000FF,1),Color(0xFFFF00,1),Color(0x00FF00,1),Color(0xff0000,1)]
+blueOutline = LineStyle(5,color[0])
+
+
+
+
+def mouseClick(event):
+    Rectangle = RectangleAsset(200,100,blueOutline,color[randint(1,4)])
+
+    Sprite(Rectangle,(75,100))
+
+
+App().listenMouseEvent('click', mouseClick)
+App().run()
