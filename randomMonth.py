c28a8301a9406c117eb68459aaaa88ab50d1ded4
maxmyth01 <maxmyth01@gmail.com>
2017-11-16 14:29:51 -0500

    Updated from Brython Server: 11/16/2017 2:29:50 PM

--- randomMonth.py
+++ randomMonth.py
@@ -1,1 +1,6 @@
+#Max Low
+#11-16-17
+#randomMonth.py -- prints a random month
+
+month = ['January','Febuary','March','April','May','June','July','August','September','October','November','December']
 
