cbb8d4bf51158a1a393ca2511ff9ea09ef4e9fc6
maxmyth01 <maxmyth01@gmail.com>
2017-11-27 14:43:13 -0500

    Updated from Brython Server: 11/27/2017 2:43:12 PM

--- cocktailSort.py
+++ cocktailSort.py
@@ -42,8 +42,13 @@ def mySort(A):
             if A[i] > A[i+1]:
                 A[i], A[i+1] = A[i+1],A[i] # swap in Python
             swapped = True
-    
-            
+        if not swapped:
+            break
+        swapped = false
+        for i in range(len(A)-2,-1,-1):
+            if A[i] > A[i+1]:
+                A[i], A[i+1] = A[i+1],A[i] # swap in Python
+                swapped = True
     return A
 
 if __name__ == '__main__':
