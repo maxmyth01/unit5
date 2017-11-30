4c260bf8911e8f5474702c0fa3a91d56af8a46da
maxmyth01 <maxmyth01@gmail.com>
2017-11-30 14:12:40 -0500

    Updated from Brython Server: 11/30/2017 2:12:39 PM

--- warmup15.py
+++ warmup15.py
@@ -1,1 +1,13 @@
+#Max Low
+#11/30/17
+#warmup15.py -- doubble the number list
 
+
+new = []
+def doubled():
+    for num in double:
+        new.append(num*2)
+    return new
+        
+
+print(doubled([2,4,6]))
