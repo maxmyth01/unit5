7e5169e58827d4e43ad8725e992fa1a8f848ca08
maxmyth01 <maxmyth01@gmail.com>
2017-12-01 14:39:36 -0500

    Updated from Brython Server: 12/1/2017 2:39:35 PM

--- stat.py
+++ stat.py
@@ -1,1 +1,11 @@
+#Max Low
+#12-1-17
+#stat.py  -- takes several inputs then gives min, max, mean, median, mode
 
+numbers = []
+
+while True:
+    num = input("Type a list of numbers, enter q to stop")
+    if num == "q":
+        
+    numbers.append(input(""))
