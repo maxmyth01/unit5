719a2b541e7c31e5b6a75156abe5aa49486e964a
maxmyth01 <maxmyth01@gmail.com>
2017-12-04 14:26:07 -0500

    Updated from Brython Server: 12/4/2017 2:26:06 PM

--- quiz5.py
+++ quiz5.py
@@ -1,1 +1,16 @@
+#Max Low
+#12-4-17
+#quiz 5.py -- 5 rand num in a list, last element of a list, number of charactures in each word, andlarges w/o using min/max
 
+from random import randint
+
+def rand5:
+    
+
+
+"""
+print(rand5())
+print(lastElement(['cat','dog','rat']))
+print(wordLengths(['the','cat,'is','hungry']))
+print(biggest([3,-1,5,-2,7,2,1]))
+"""
