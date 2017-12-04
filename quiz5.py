d99ac06a57ceeb47d5c18d9b6c6bce076e1e7f9e
maxmyth01 <maxmyth01@gmail.com>
2017-12-04 14:27:43 -0500

    Updated from Brython Server: 12/4/2017 2:27:42 PM

--- quiz5.py
+++ quiz5.py
@@ -5,11 +5,17 @@
 from random import randint
 
 def rand5:
-    
+    x=0
+    rand = []
+    while x < 5:
+        rand.append randint(1,100)
+        x+=1
+    return rand
 
 
-"""
+
 print(rand5())
+"""
 print(lastElement(['cat','dog','rat']))
 print(wordLengths(['the','cat,'is','hungry']))
 print(biggest([3,-1,5,-2,7,2,1]))
