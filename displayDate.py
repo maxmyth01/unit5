e2df20ec990b5211185186913752e19b465492fe
maxmyth01 <maxmyth01@gmail.com>
2017-12-03 21:59:55 -0500

    Updated from Brython Server: 12/3/2017 9:59:54 PM

--- displayDate.py
+++ displayDate.py
@@ -5,17 +5,15 @@
 from datetime import date
 from calendar import weekday
 
+# set todays date, to then pull out various peices from it
 today = date.today()
-day = today.day
-month = today.month
-year = today.year
-week = today.weekday()
 
-
+# list of the months and days of the week to convert the numerical value to text
 dayOfWeek = ["Monday","Tuesday","Wenesday","Thursday","Friday","Saterday","Sunday"]
 monthOfYear = ["January","Febuary","March","April","May","June","July","Auguest","September","October","November","December"]
 
-weeks = dayOfWeek[week]
-months = monthOfYear[month-1]
+# the numerial day of the week function acounts for programs starting counting on 0, the months of the year do not
+weeks = dayOfWeek[today.weekday()]
+months = monthOfYear[today.month-1]
 
-print("Today is",weeks,months,day,year)
+print("Today is",weeks,months,today.day,today.year)
